01c5437630be2de9c14c64047334f70fb9ace66e
Worameth Suwannapruuk <nut.worameth@gmail.com>
2026-09-01 15:16:02 +0700

    A5 v1

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+app.use(express.json());
 
 const hospitals = require("./routes/hospitals");
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5003;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
 });
